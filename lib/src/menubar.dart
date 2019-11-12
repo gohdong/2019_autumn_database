21c2e1204d911cc1defe20324022c0c5ecf447a4
aldehf420 <aldehf420@naver.com>
2019-11-12 14:43:13 +0900

    flutter init

---
+++ lib/src/menubar.dart
@@ -0,0 +1,40 @@
+import 'package:dbapp/main.dart';
+import 'package:flutter/material.dart';
+
+
+class MenuBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: ListTile(
+              title: Text('Login',textScaleFactor: 1.3,),
+              onTap: () {
+              },
+            ),
+            decoration: BoxDecoration(
+              color: Colors.black45,
+            ),
+
+          ),
+          ListTile(
+            title: Text('Home',textScaleFactor: 1.5,),
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => MyApp()));
+            },
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Notice',textScaleFactor: 1.5,),
+            onTap: () {
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
