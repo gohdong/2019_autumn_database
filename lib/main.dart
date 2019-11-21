73d9283ea3f5d6a9df5851298af714c859a40297
jonghwi Wang <wangjh789@daum.net>
2019-11-21 15:35:30 +0900

    add logout

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:provider/provider.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'src/store.dart';
 
 void main() => runApp(MyApp());
--- lib/src/moviechart.dart
+++ lib/src/moviechart.dart
@@ -1,7 +1,6 @@
 import 'package:dbapp/src/moviepage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/diagnostics.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MovieChart extends StatefulWidget {
