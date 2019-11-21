d02a8cd5d9ed500df17c10abd4b0933d68de3828
KimDongHyun <kdh2637@hanyang.ac.kr>
2019-11-22 01:31:10 +0900

    modify Store & add func to make structure

---
+++ lib/src/StoreCombo.dart
@@ -0,0 +1,117 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StoreCombo extends StatelessWidget {
+  StoreCombo({this.repeat = 0});
+
+  int repeat;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        child: Row(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  child: getComboImage(),
+                  width: 100,
+                  height: 100,
+                )
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[getComboName()],
+                ),
+//              or text
+                Row(
+                  children: <Widget>[getComboDescription()],
+                ),
+//              or text
+                Row(
+                  children: <Widget>[
+//                  Column(children: <Widget>[getComboPrice2()],),
+//              or text
+                    Column(
+                      children: <Widget>[getComboPrice1()],
+                    ),
+//              or text
+                  ],
+                )
+//              or text
+              ],
+            ),
+          ],
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+
+  Widget getComboImage() {
+    return new StreamBuilder(
+      stream: Firestore.instance.collection('store_com').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return new Text("Cannot Found..");
+        }
+        return new Image.network(snapshot.data.documents[repeat]['img']);
+//        return new Text(snapshot.data.documents[repeat]['name']);
+      },
+    );
+  }
+
+  Widget getComboName() {
+    return new StreamBuilder(
+      stream: Firestore.instance.collection('store_com').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return new Text("Cannot Found..");
+        }
+        return new Text(snapshot.data.documents[repeat]['name']);
+      },
+    );
+  }
+
+  Widget getComboDescription() {
+    return new StreamBuilder(
+      stream: Firestore.instance.collection('store_com').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return new Text("Cannot Found..");
+        }
+        return new Text(snapshot.data.documents[repeat]['desc']);
+      },
+    );
+  }
+
+  Widget getComboPrice1() {
+    return new StreamBuilder(
+      stream: Firestore.instance.collection('store_com').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return new Text("Cannot Found..");
+        }
+        return new Text(snapshot.data.documents[repeat]['price']);
+      },
+    );
+  }
+
+//  Widget getComboPrice2() {
+//    return new StreamBuilder(
+//      stream: Firestore.instance
+//          .collection('store_com')
+//          .snapshots(),
+//      builder: (context, snapshot) {
+//        if (!snapshot.hasData) {
+//          return new Text("Cannot Found..");
+//        }
+//        return new Text(snapshot.data.documents[repeat]['price2']);
+//      },
+//    );
+//  }
+}
--- lib/src/store.dart
+++ lib/src/store.dart
@@ -1,78 +1,111 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dbapp/src/StorePackage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'StoreCombo.dart';
+import 'StoreGiftcard.dart';
+import 'StoreRow.dart';
+import 'StoreTicket.dart';
 
+// 리스트뷰
 class Store extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: ListView(children: <Widget>[
       ListTile(
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[Text("TICKET & POPCORN STORE")],
-        ),
-        onTap: () {
-//            Navigator.of(context)
-//                .push(MaterialPageRoute(builder: (context) => null));
-        },
-      ),
+          title: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  child: StreamBuilder(
+                    stream: Firestore.instance
+                        .collection('store_main')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return new Text("Cannot Found..");
+                      }
+                      return new Image.network(snapshot.data.documents[0]['header']);
+                    }
+                  ),
+                )
+              ]),
+          onTap: () {}),
       Divider(color: Colors.black),
       ListTile(
           title: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-            Text("패키지"),
-            Text("영화관람권"),
-            Text("콤보"),
-            Text("기프트카드")
+                InkWell(child: Text("패키지"),onTap: (){},),
+                InkWell(child: Text("영화관람권"),onTap: (){},),
+                InkWell(child: Text("콤보"),onTap: (){},),
+                InkWell(child: Text("기프트카드"),onTap: (){},)
           ])),
       Divider(color: Colors.black),
 
-      ListTile(
-          title: Column(children: <Widget>[
-        Row(children: <Widget>[Text("고객님께 추천하는 영화볼 때 필수 아이템")]),
-        Row(children: <Widget>[
-          Column(children: <Widget>[
-            Row(children: <Widget>[Text("제품 명")]),
-            Row(children: <Widget>[Text("설명")]),
-            Row(children: <Widget>[Text("가격")])
-          ]),
-          Column(children: <Widget>[Container(child: Text("사진"))])
-        ])
-      ])),
-
-      Divider(color: Colors.black),
+//      ListTile(
+//          title: Column(children: <Widget>[
+//        Row(children: <Widget>[Text("고객님께 추천하는 영화볼 때 필수 아이템")]),
+//        Row(children: <Widget>[
+//          Column(children: <Widget>[
+//            Row(children: <Widget>[Text("제품 명")]),
+//            Row(children: <Widget>[Text("설명")]),
+//            Row(children: <Widget>[Text("가격")])
+//          ]),
+//          Column(children: <Widget>[Container(child: Text("사진"))])
+//        ])
+//      ])),
+//
+//      Divider(color: Colors.black),
 
       // 베스트 상품, 선물추천, 팝콘, 음료, 스낵      추가하기
-
-      StoreRow(),
+//      StoreProduct(),
+      makePackage(),
       Divider(color: Colors.black),
-      StoreRow(),
+      makeTicket(),
       Divider(color: Colors.black),
-      StoreRow(),
+      makeCombo(),
       Divider(color: Colors.black),
-      StoreRow(),
+      makeGiftcard(),
       Divider(color: Colors.black),
       Card(child: Center(child: Text("이용안내")))
     ]));
   }
 }
 
-Widget StoreRow() {
-  return ListTile(
-    title: Column(
-      children: <Widget>[
-        Row(children: <Widget>[Text("카테고리")]),
-        for (var i = 0; i < 3; i++)
-          Row(children: <Widget>[
-            Column(children: <Widget>[Container(child: Text("사진"))]),
-            Column(children: <Widget>[
-              Row(children: <Widget>[Text("카테고리 명")]),
-              Row(children: <Widget>[Text("설명")]),
-              Row(children: <Widget>[Text("가격")])
-            ])
-          ]),
-      ],
-    ),
+Widget makePackage(){
+  return Column(
+    children: <Widget>[
+      for (int i = 0; i < 3; i++)
+        StorePackage(repeat: i)
+    ],
+  );
+}
+
+Widget makeTicket(){
+  return Column(
+    children: <Widget>[
+      for (int i = 0; i < 3; i++)
+        StoreTicket(repeat: i)
+    ],
+  );
+}
+
+Widget makeCombo(){
+  return Column(
+    children: <Widget>[
+      for (int i = 0; i < 3; i++)
+        StoreCombo(repeat: i)
+    ],
+  );
+}
+
+Widget makeGiftcard(){
+  return Column(
+    children: <Widget>[
+      for (int i = 0; i < 3; i++)
+        StoreGiftcard(repeat: i)
+    ],
   );
 }
