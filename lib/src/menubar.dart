edef17e8a1e37552ef72d77f4e89a3d2a004b4ca
Source_SC <kimdh425@naver.com>
2019-12-05 16:20:13 +0900

    remove menubar adding function

--- lib/src/menubar.dart
+++ lib/src/menubar.dart
@@ -207,19 +207,19 @@ class MenuBar extends StatelessWidget {
           gridComponent(Icons.credit_card, '할인정보'),
           gridComponent(Icons.info, '고객센터'),
           gridComponent(Icons.stars, "특별관"),
-//          InkWell(
-//              onTap: () {
-//                Navigator.of(context)
-//                    .push(MaterialPageRoute(builder: (context) => MyApp()));
-//              },
-//              child: gridComponent(Icons.event, "이벤트")),
           InkWell(
               onTap: () {
                 Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => prefix0.Make_seat()));
+                    .push(MaterialPageRoute(builder: (context) => MyApp()));
               },
-              child: gridComponent(Icons.event, "추가")),
-
+              child: gridComponent(Icons.event, "이벤트")),
+//          InkWell(
+//              onTap: () {
+//                Navigator.of(context)
+//                    .push(MaterialPageRoute(builder: (context) => prefix0.Make_seat()));
+//              },
+//              child: gridComponent(Icons.event, "추가")),
+//
 
           gridComponent(Icons.collections_bookmark, "매거진"),
           gridComponent(Icons.event_seat, "club서비스"),
